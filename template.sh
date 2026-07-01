da40e701cc0ea5a045e8f81188f0ee853adf7631
Karunakar <karunakarthurpati154@gmail.com>
2026-07-01 11:37:08 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+
+touch src/_init_.py
+touch src/helper.py
+touch src/prompclear.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+echo "directory files created sucessfully"
